17695238bae902fe230c2bbcac5bf1b753447f3e
benjavitorgan <vitorganbenjamin@gmail.com>
2022-05-10 15:38:04 -0300

    Numeric input field

--- Assets/Scripts/DataInputManager.cs
+++ Assets/Scripts/DataInputManager.cs
@@ -6,11 +6,22 @@ using UnityEngine.UI;
 public class DataInputManager : MonoBehaviour
 {
     public Text txtUserName;
+    
 
     //Elimino funciones default
 
     public void ShowGreetings ()
     {
         Debug.Log("Hola " + txtUserName.text);
+    }
+
+    public void ShowDouble () {
+        if (txtUserName.text != "")
+        {
+            Debug.Log(int.Parse(txtUserName.text) * 2);
+        } else
+        {
+            Debug.Log ("El campo no puede estar vacio");
+        }
     }
 }
