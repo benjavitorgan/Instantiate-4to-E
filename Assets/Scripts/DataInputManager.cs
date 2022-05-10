b42d62003ff31a814101d3839ee95282908b9688
benjavitorgan <vitorganbenjamin@gmail.com>
2022-05-10 15:27:08 -0300

    Input field for text

---
+++ Assets/Scripts/DataInputManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DataInputManager : MonoBehaviour
+{
+    public Text txtUserName;
+
+    //Elimino funciones default
+
+    public void ShowGreetings ()
+    {
+        Debug.Log("Hola " + txtUserName.text);
+    }
+}
